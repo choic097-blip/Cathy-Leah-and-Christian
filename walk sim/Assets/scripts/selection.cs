9b477f3fedb3930e7b5b2786333a8ff643d38af8
exe1vy <1meluvfri1@gmail.com>
2026-02-17 11:49:55 -0500

    qwdeqfwef

---
+++ walk sim/Assets/scripts/selection.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class selection : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
