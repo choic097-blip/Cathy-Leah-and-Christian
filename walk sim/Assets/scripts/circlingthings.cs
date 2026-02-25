3f8ab4a73107528dc8d34f0327dec45c26e5df63
exe1vy <1meluvfri1@gmail.com>
2026-02-24 20:55:44 -0500

    rfubiunoip

---
+++ walk sim/Assets/scripts/circlingthings.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using Random = UnityEngine.Random;
+
+
+public class circlingthings : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 2f;
+    public float radius = 1f;
+    public float angle = 0f;
+    void Start() 
+    {
+        angle = Random.Range(1f,360f);
+    }
+    void Update()
+    {
+        float x = target.position.x;
+        float y = target.position.y + Mathf.Cos(angle) * radius;
+        float z = target.position.z + Mathf.Sin(angle) * radius;
+
+        transform.position = new Vector3(x, y, z);
+
+        angle += speed * Time.deltaTime;
+    }
+}
